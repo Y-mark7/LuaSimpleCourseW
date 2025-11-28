ecbd2107b7e8aefd3c70bb4b0eb7fc9e2e8645f7
Y-mark7 <mark.yamaguti7@gmail.com>
2025-11-28 12:22:10 -0300

    Aula 1

---
+++ Studies/Aula1.lua
@@ -0,0 +1,31 @@
+print("Olá Mundo!")
+print("Bem-vindo ao Lua!")
+
+print("12345")
+print(12345)
+
+print("------------------------------------------")
+print(" ")
+--Comentário de uma linha
+
+--[[Comentário
+de múltiplas
+linhas]]--
+
+--Operadores Aritméticos
+print("Soma: 10 + 2 =", 10 + 2)
+print("Subtração: 30 - 5 =", 30 - 5)
+print("Multiplicação: 3 * 7 =", 3 * 7)
+print("Divisão: 20 / 4 =", 20 / 4)
+print("Módulo: 9 % 3 =", 9 % 3)
+
+--Operadores Lógicos
+
+print("2 > 3?")
+print(2 > 3)
+
+print("2 < 3?")
+print(2 < 3)
+
+print("3 == 3?")
+print(3 == 3)
