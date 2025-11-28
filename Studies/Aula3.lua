82352ccbedb43f3c7bef65735c6a202a5452a365
Y-mark7 <mark.yamaguti7@gmail.com>
2025-11-28 12:56:55 -0300

    Aula 3

---
+++ Studies/Aula3.lua
@@ -0,0 +1,11 @@
+local estaAtivo = false
+print("Valor de estaAtivo = " .. tostring(estaAtivo))
+print(estaAtivo == false)
+
+local estaInativo = true
+print("Valor de estaInativo = " .. tostring(estaInativo))
+print(estaInativo == true)
+
+print("-------------------------")
+
+print("Valor de 123 = " .. tostring(123))
