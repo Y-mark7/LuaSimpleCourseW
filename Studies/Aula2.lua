bec01bd064daaa5f771fd4346f968db4d34b158e
Y-mark7 <mark.yamaguti7@gmail.com>
2025-11-28 12:27:42 -0300

    Aula 2

---
+++ Studies/Aula2.lua
@@ -0,0 +1,24 @@
+local olaMundo = "Olá Mundo"
+print(olaMundo)
+
+olaMundo = "Olá Mundo Modificado"
+print(olaMundo)
+
+local legal = 42
+print(legal)
+
+print("-------------------------------------")
+
+local linha1 = "Primeira linha do texto"
+local linha2 = "Segunda linha do texto"
+print(linha1)
+print(linha2)
+print(linha1 .. linha2)
+
+local part1 = "Primeira parte"
+local part2 = "Segunda parte"
+print(part1 .. " " .. part2)
+
+local valor = 450
+local frase = "O valor total é: "
+print(frase .. "R$ " .. valor)
