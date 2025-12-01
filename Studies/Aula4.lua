c31cb6622d13cee95bab6d37a2c25e88fcc34489
Y-mark7 <mark.yamaguti7@gmail.com>
2025-12-01 12:42:46 -0300

    Aula 4

---
+++ Studies/Aula4.lua
@@ -0,0 +1,41 @@
+local resultado = 6 - 10
+print("O resultado de 6 - 10 é: " .. resultado)
+
+resultado = -2
+print("O valor negativo de 2 é: " .. resultado)
+
+local numero = 45.5
+print("O valor de número é: " .. numero)
+
+local outroNumero = -3.6
+print("O valor de outroNúmero é: " .. outroNumero)
+
+local pi = 3.141592653589793
+print("O valor de pi é: " .. pi)
+
+local nada = nil
+print(nada)
+
+nada = "Agora nada tem um valor!"
+print(nada)
+
+nada = nil
+print(tostring(nada == "Agora nada tem um valor!"))
+
+local algo = "Caixa D'água"
+print(algo)
+
+local frase = "O nome dela é \"Laura\""
+print(frase)
+
+local barra = "Texto com barra \\"
+print(barra)
+
+local proximaLinha = "Primeira linha\nSegunda linha"
+print(proximaLinha)
+
+local tabulacao = "\ttabulação antes de mim"
+print(tabulacao)
+
+local retorno = "\t\reai"
+print(retorno)
