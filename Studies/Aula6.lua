5150c139ef79c8419b7ce9e94079b229bc7ffa6c
Y-mark7 <mark.yamaguti7@gmail.com>
2025-12-02 11:29:42 -0300

    Aula 6

---
+++ Studies/Aula6.lua
@@ -0,0 +1,14 @@
+local function somarUm()
+    print(1+1)
+end
+
+somarUm()
+
+local function somar(n1, n2)
+    print("Soma de " .. n1 .. " com " .. n2 .. " = " .. (n1 + n2))
+end
+
+somar(2, 4)
+somar(43, 12)
+somar(7.1, 4.3)
+somar(4, -6)
